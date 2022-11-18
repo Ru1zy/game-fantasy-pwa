890f4e261ad68d88c32e7a53fb6a0bc8da6ea3d3
unknown <unruized@gmail.com>
2022-11-18 15:09:10 +0200

    Throw Exception

--- src/js/Character.js
+++ src/js/Character.js
@@ -19,6 +19,9 @@ export default class Character {
     this.defence = 0;
     this.health = 50;
     this.type = type;
-    // TODO: выбросить исключение, если кто-то использует "new Character()"
+    // выбросить исключение, если кто-то использует "new Character()"
+    if (new.target.name === "Character") {
+      throw new Error("Dude, stop call dat. Call already created characters.");
+    }
   }
 }
