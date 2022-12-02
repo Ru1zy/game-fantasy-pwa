d48d1672a798e55645fafbfc56dc9f2b082495c9
Devilman1488 <arondil.sigmar@protonmail.com>
2022-12-02 18:39:46 +0200

    [fix] wrong path to service-worker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./js/app";
 // Точка входа webpack
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    navigator.serviceWorker.register('./service-worker.js').then(registration => {
       console.log('SW registered: ', registration);
     }).catch(registrationError => {
       console.log('SW registration failed: ', registrationError);
