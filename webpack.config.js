bfa5531201c03aed165103958ba426cffa3c3166
Ru1zy <unruized@gmail.com>
2026-09-19 10:36:04 +0300

    ci: add automatic gh-pages deploy script and GitHub Actions workflow

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'trash.js',
+    publicPath: 'auto',
   },
   devServer: { open: { app: { name: 'firefox' } } },
   module: {
